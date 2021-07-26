55836dbc606acecaf704a932ab18a9241b761b36
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-07-26 15:17:45 -0700

    Change background image from img element to css property
    associate background-image to pageWrapper class.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import { animals } from './animals';
 import React from 'react';
 import './App.css';
 
 const showBackground = true;
-const background = <img className="background" alt="ocean" src="/images/ocean.jpg"/>
+const background = <img className="background" alt="ocean" src="/images/backgroundImage.png"/>
+
+const title = ""
+
+const images = [];
+for (const animal in animals) {
+   images.push(
+    <img 
+      onClick={displayFact} 
+      key={animal} 
+      className='animal' 
+      alt={animal} 
+      src={animals[animal].image} 
+      aria-label={animal} 
+      role='button'
+    />
+  )
+}
 
 function displayFact(e) {
   const selectedAnimal = e.target.alt;
@@ -36,31 +28,18 @@ function displayFact(e) {
   const optionIndex = Math.floor(Math.random() * animalInfo.facts.length);
   const funFact = animalInfo.facts[optionIndex];
   document.getElementById('fact').innerHTML = funFact;
-}
-
-function displayBG(e) {
-  const selectedAnimal = e.target.alt;
-
-}
-
-const images = [];
-for (const animal in animals) {
-   images.push(<img onClick={displayFact} key={animal} className='animal' alt={animal} src={animals[animal].image} aria-label={animal} role='button'/>)
-}
-
-
-const title = ""
+};
 
 function App() {
   return (
-    <div>
-    <h1>{!title && "Click an animal for a fun fact"}</h1>
-    {showBackground && background}
-    <p id='fact'></p>
-    <div className='animals'>{images}</div>
-    <input type='button' value="Click me" onClick={displayBG}/>
-  </div>
+    <div className="pageWrapper">
+      <h1 className="pageTitle">{!title && "Click an animal for a fun fact"}</h1>
+      {/* {showBackground && background} */}
+      <p id='fact'></p>
+      <div className='animals'>{images}</div>
+      {/* <input type='button' value="Click me" onClick={displayBG}/> */}
+    </div>
   );
-}
+};
 
 export default App;
